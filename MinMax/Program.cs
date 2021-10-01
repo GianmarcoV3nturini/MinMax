f15c1dbf8975f7e786a58bceea549c4fde506004
Gianmarco Venturini <GIANMARCO.VENTURINI@local.ittcesena.it>
2021-10-01 09:32:34 +0200

    V1.1: aggiunto ciclo non funzionante per ricerca minimo

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -10,7 +10,18 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
-            
+            int[] vettore = new int[5] { 4, 6, -3, 5, 7 };
+
+            int min = vettore[0];
+
+            for(int i=0; i>5;i++)
+            {
+                if (vettore[i] < min)
+                    min = vettore[i];
+            }
+
+            Console.WriteLine(min);
+            Console.ReadKey();
         }
     }
 }
